2a395ea631ec5b7847b2a93a8ede79a9d3bb96b2
B00153330 Caoimhe Walsh <B00153330@mytudublin.ie>
2024-11-19 15:39:51 +0000

    part 4

--- processForm1.php
+++ processForm1.php
@@ -5,7 +5,7 @@ $_SESSION['passengerFN'] = $_POST['firstname'];
 $_SESSION['passengerSN'] = $_POST['surname'];
 
 if (isset($_POST['luggage']))  {
-    $_SESSION['luggage'] = 0;
+    $_SESSION['luggage'] = 1;
     header("Location: luggage.html");
 
 
